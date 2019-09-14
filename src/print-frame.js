39dbe401d88ceae549299bcf3a86d7bed3ddd449
Colin Tester <github@z-omo.com>
2019-09-14 13:13:38 +0100

    Establish dev environment and PrintFrame src files.

---
+++ src/print-frame.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const PF = {
+
+  view: {
+    frameID:  'printFrame'
+  },
+
+  printThis(view, callback, title)
+  {
+    prepareFrame();
+
+    const style = 'position:fixed;bottom:-100%;left:-100%;';
+    PF.view.frame.setAttribute('style', style);
+
+    PF.view.frame.addEventListener(
+      'load', () => { callToPrint(); }, { once: true }
+    );
+
+    if (title) { title = formatFileSafeTitle(title); }
+    setFrameView(view, title);
+
+    PF.view.frame.contentWindow.addEventListener('afterprint', () => {
+      afterPrint();
+      if (callback) { callback(); }
+    }, { once: true });
+  },
+
+  remove()
+  {
+    removePrintFrame();
+  }
+};
+
+export default PF;
+
+function prepareFrame(container = document.body)
+{
+  let frame = document.getElementById(PF.view.frameID);
+  if (!frame) { frame = buildFrame(); }
+
+  container.appendChild(frame);
+  PF.view.frame = frame;
+}
+
+function buildFrame()
+{
+  const frame = document.createElement('iframe');
+  frame.id = PF.view.frameID;
+
+  return frame;
+}
+
+function setFrameView(view, title)
+{
+  const frame = PF.view.frame;
+  if (!frame) { return; }
+
+  frame.contentWindow.document.open();
+  frame.contentWindow.document.write(view);
+  if (title) { setDocumentTitle(title); }
+  frame.contentWindow.document.close();
+}
+
+function callToPrint()
+{
+  const frame = PF.view.frame;
+
+  if (!frame)
+  {
+    throw new Error('Cannot action Print command without a prepared print frame.');
+  }
+
+  const result = frame.contentWindow.document.execCommand('print', true);
+  if (false === result) { frame.contentWindow.print(); }
+}
+
+function afterPrint()
+{
+  // reset Chrome browser parent document title.
+  if (PF.view.chromeTitle)
+  {
+    PF.view.frame.contentWindow.parent.document.title = PF.view.chromeTitle;
+    PF.view.chromeTitle = '';
+  }
+}
+
+function removePrintFrame()
+{
+  prepareFrame();
+  PF.view.frame.remove();
+  PF.view.frame = null;
+}
+
+function formatFileSafeTitle(string)
+{
+  let safe = string.normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/&(amp;)?/g, ' and ')
+    .replace(/[^0-9a-z\s-–—]/gi, '')
+    .replace(/[\s–—]+/g, '-');
+
+  return safe;
+}
+
+function setDocumentTitle(title)
+{
+  const frame = PF.view.frame;
+  frame.contentWindow.document.title = title;
+
+  const chrome = /Chrome\//.test(navigator.userAgent)
+        && /Google Inc/.test(navigator.vendor);
+  if (!chrome) { return; }
+
+  PF.view.chromeTitle = frame.contentWindow.parent.document.title;
+  frame.contentWindow.parent.document.title = title;
+}
---
+++ src/tests/print-frame-test.js
@@ -0,0 +1,76 @@
+import test from 'ava';
+import PF from '../print-frame.js';
+
+test('PrintFrame imported is an object', t => {
+  t.is(typeof PF, 'object');
+});
+
+test('PrintFrame object has a view property object', t => {
+  t.is(typeof PF.view, 'object');
+});
+
+test('PrintFrame provides public methods', t => {
+  t.is(typeof PF.printThis, 'function');
+  t.is(typeof PF.remove, 'function');
+});
+
+const content = '<h1>Print Test</h1>';
+let frame = null;
+
+test('PrintFrame builds iframe element into the DOM', t => {
+  PF.printThis(content);
+
+  frame = document.querySelector('#' + PF.view.frameID);
+  t.is(frame.nodeName, 'IFRAME')
+  t.is(PF.view.frame, frame);
+});
+
+test('PrintFrame hides inserted iframe from view', t => {
+  frame = document.querySelector('#' + PF.view.frameID);
+
+  const style = frame.getAttribute('style');
+  t.is(style, 'position:fixed;bottom:-100%;left:-100%;');
+});
+
+test('PrintFrame sets iframe content correctly', t => {
+  const frameContent = frame.contentWindow.document.body.innerHTML;
+  t.is(typeof frameContent, 'string');
+  t.is(frameContent, content);
+});
+
+test('PrintFrame removes iframe element from DOM', t => {
+  PF.remove();
+
+  const removed = document.querySelector('#' + PF.view.frameID);
+  t.not(frame, removed);
+  t.not(PF.view.frame, frame);
+});
+
+test('PrintFrame sets iframe document title to a URL safe filename', t => {
+  const testTitles = [
+    'Foo &amp; Bar', 'Tést—001 for fü bar', 'Wow! that was "great!?"'
+  ];
+
+  const expected = [
+    'Foo-and-Bar', 'Test-001-for-fu-bar', 'Wow-that-was-great'
+  ];
+
+  testTitles.forEach((title, index) => {
+    PF.printThis(content, undefined, title);
+    frame = document.querySelector('#' + PF.view.frameID);
+    noPrintDialog(frame);
+
+    const docTitle = frame.contentWindow.document.title;
+
+    t.is(typeof docTitle, 'string');
+    t.is(docTitle, expected[index]);
+  });
+});
+
+function noPrintDialog(frame)
+{
+  const dummy = () => { return; }
+  frame.contentWindow.print = dummy;
+  frame.contentWindow.document.execCommand = dummy;
+}
+
